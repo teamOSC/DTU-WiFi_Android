076490ae10a284fb16ae9fe2ae29d530e9de6ad9
Prempal Singh <prempal.42@gmail.com>
2015-08-20 13:28:07 +0530

    login works

--- app/src/main/java/osc/dtuwifi/MainActivity.java
+++ app/src/main/java/osc/dtuwifi/MainActivity.java
@@ -1,16 +1,71 @@
 package osc.dtuwifi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final WebView webView = (WebView) findViewById(R.id.webView);
+        final EditText username = (EditText) findViewById(R.id.username);
+        final EditText password = (EditText) findViewById(R.id.password);
+        Button submit = (Button) findViewById(R.id.submit);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Log.d(TAG, url);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                Log.d(TAG, "Error : " + error.getErrorCode() + "");
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+                Log.d(TAG, "SSL Error : " + error.toString());
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.loadUrl("https://10.50.0.100/connect/PortalMain");
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.loadUrl("javascript: {" +
+                        "document.getElementById('LoginUserPassword_auth_username').value = '" + username.getText().toString() + "';" +
+                        "document.getElementById('LoginUserPassword_auth_password').value = '" + password.getText().toString() + "';" +
+                        "var button = document.getElementById('UserCheck_Login_Button');" +
+                        "button.click(); };");
+            }
+        });
+
     }
 
     @Override
